ea7f9384fccbd8e370f53c097432510749674528
unknown <robertishimwe0@gmail.com>
2024-03-12 21:46:08 +0000

    added api integration of unit and category

--- src/components/productManagement/dataGrid.jsx
+++ src/components/productManagement/dataGrid.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem, DropdownMenuContent, DropdownMenu } from "@/components/ui/dropdown-menu"
 import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card"
 import { TableHead, TableRow, TableHeader, TableCell, TableBody, Table } from "@/components/ui/table"
 
+import { useAtom } from 'jotai';
+import { productsAtom } from "../../utils/atoms";
+
+import api from "../../utils/api";
+
 function DataGrid() {
+
+ const [products, setProducts] = useAtom(productsAtom);
+
+ const { isLoading, isError, data, error } = useQuery({
+  queryKey: ['products'],
+  queryFn: async () => {
+    const response = await api.get('/api/product/getAll');
+    return response?.data;
+  }
+})
+
+
+if (isLoading) {
+  return <p className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">Loading...</p>
+}
+
+if (isError) {
+  return <p>Error: {error.message}</p>
+}
+
+if (data) {
+  setProducts(data);
+}
+
+console.log(products);
+
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
     <Card>
@@ -29,19 +61,20 @@ function DataGrid() {
                 <TableHead>Action</TableHead>
               </TableRow>
             </TableHeader>
-            <TableBody>
+            <TableBody>{
+              products && products.map((product) => (
               <TableRow>
-                <TableCell>simerwa cement 0.9</TableCell>
-                <TableCell>shgfkhasgdfkausydgfkyudgfk</TableCell>
-                <TableCell>1</TableCell>
-                <TableCell>1</TableCell>
-                <TableCell>1</TableCell>
-                <TableCell>$10,000</TableCell>
+                <TableCell>{product.product_name}</TableCell>
+                <TableCell>{product.description}</TableCell>
+                <TableCell>{product?.productCategory?.name}</TableCell>
+                <TableCell>{product?.Supplier?.supplierName}</TableCell>
+                <TableCell>{product?.unit?.unit_name}</TableCell>
+                <TableCell>{product?.price} RFW</TableCell>
                 <TableCell>
                   <Button variant="ghost">Edit</Button>
                   <Button variant="ghost">Delete</Button>
                 </TableCell>
-              </TableRow>
+              </TableRow>))}
             </TableBody>
           </Table>
         </CardContent>
