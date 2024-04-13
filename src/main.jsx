23f5c2f2bf44d539b3863a7d2cbec4e4ea1a911a
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2024-04-13 16:03:32 +0000

    fixed inventory Id

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { Toaster } from 'react-hot-toast';
 import App from "./App.jsx";
 import "./index.css";
