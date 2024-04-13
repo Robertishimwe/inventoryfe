554e202b2b92d95b09c3b51aa8e2e43f63589915
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2024-04-13 16:02:27 +0000

    fixed inventory Id

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Toaster } from 'react-hot-toast';
 import App from "./App.jsx";
 import "./index.css";
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <QueryClientProvider client={queryClient}>
       <Toaster />
         <App />
+        <SpeedInsights/>
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
