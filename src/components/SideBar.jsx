5a498c052bb015898d75b1d80d05795f6c2573ac
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2024-08-14 07:05:29 +0000

    added report icon

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -81,7 +81,7 @@ function SideBar() {
               className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
               to="/dashboard/report/main" style={{color:"#000"}}
             >
-              <DollarSignIcon className="h-4 w-4" />
+              <ReportIcon className="h-4 w-4" />
               Reports
             </Link>            
           </nav>
@@ -325,5 +325,29 @@ function UsersIcon(props) {
     </svg>
   );
 }
+
+function ReportIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+      <polyline points="14 2 14 8 20 8" />
+      <line x1="16" y1="13" x2="8" y2="13" />
+      <line x1="16" y1="17" x2="8" y2="17" />
+      <polyline points="10 9 9 9 8 9" />
+    </svg>
+  );
+}
+
 
 export default SideBar;
